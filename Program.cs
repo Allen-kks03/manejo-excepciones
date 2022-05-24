31a47981a72152476189ccff737ad041ba499845
Allen Jacob Suarez Briseño <l21212056@tectijuana.edu.mx>
2022-05-24 11:18:30 -0700

    manejo-excepciones
    fuchi

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace manejo_excepciones
     {
         static void Main(string[] args)
         {
+            int Num;
+
+            try
+            {
+                Console.Write("Dame un numero entero: ");
+                Num = int.Parse(Console.ReadLine());
+                Console.WriteLine("El cuadrado es: "+Math.Pow(Num, 2));
+                Console.ReadKey();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
