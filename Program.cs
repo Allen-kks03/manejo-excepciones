7472de55f93ae2e6b6148f88dce04e571d331a03
Allen Jacob Suarez Briseño <l21212056@tectijuana.edu.mx>
2022-05-24 11:01:30 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejo_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
